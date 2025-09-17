2f804a17e0ec2ae7f8a3d2b2afc29b78e95bf01f
Mario joaquin <mgalicia@denumeris.com>
2025-09-16 22:51:39 -0600

    variables y colores de marca

--- js/child-theme.js
+++ js/child-theme.js
@@ -1,6 +1,6 @@
 /*!
   * Understrap v1.2.0 (https://understrap.com)
-  * Copyright 2013-2022 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
+  * Copyright 2013-2025 The Understrap Authors (https://github.com/understrap/understrap/graphs/contributors)
   * Licensed under GPL-3.0 (undefined)
   */
 (function (global, factory) {
@@ -224,7 +224,6 @@
 			  const reflow = element => {
 			    element.offsetHeight; // eslint-disable-line no-unused-expressions
 			  };
-
 			  const getjQuery = () => {
 			    if (window.jQuery && !document.body.hasAttribute('data-bs-no-jquery')) {
 			      return window.jQuery;
@@ -3636,44 +3635,44 @@
 
 	var lib = /*#__PURE__*/Object.freeze({
 		__proto__: null,
-		popperGenerator: popperGenerator,
-		detectOverflow: detectOverflow,
-		createPopperBase: createPopper$2,
-		createPopper: createPopper,
-		createPopperLite: createPopper$1,
-		top: top,
-		bottom: bottom,
-		right: right,
-		left: left,
-		auto: auto,
-		basePlacements: basePlacements,
-		start: start,
-		end: end,
-		clippingParents: clippingParents,
-		viewport: viewport,
-		popper: popper,
-		reference: reference,
-		variationPlacements: variationPlacements,
-		placements: placements,
-		beforeRead: beforeRead,
-		read: read,
-		afterRead: afterRead,
-		beforeMain: beforeMain,
-		main: main,
 		afterMain: afterMain,
-		beforeWrite: beforeWrite,
-		write: write,
+		afterRead: afterRead,
 		afterWrite: afterWrite,
-		modifierPhases: modifierPhases,
 		applyStyles: applyStyles$1,
 		arrow: arrow$1,
+		auto: auto,
+		basePlacements: basePlacements,
+		beforeMain: beforeMain,
+		beforeRead: beforeRead,
+		beforeWrite: beforeWrite,
+		bottom: bottom,
+		clippingParents: clippingParents,
 		computeStyles: computeStyles$1,
+		createPopper: createPopper,
+		createPopperBase: createPopper$2,
+		createPopperLite: createPopper$1,
+		detectOverflow: detectOverflow,
+		end: end,
 		eventListeners: eventListeners,
 		flip: flip$1,
 		hide: hide$1,
+		left: left,
+		main: main,
+		modifierPhases: modifierPhases,
 		offset: offset$1,
+		placements: placements,
+		popper: popper,
+		popperGenerator: popperGenerator,
 		popperOffsets: popperOffsets$1,
-		preventOverflow: preventOverflow$1
+		preventOverflow: preventOverflow$1,
+		read: read,
+		reference: reference,
+		right: right,
+		start: start,
+		top: top,
+		variationPlacements: variationPlacements,
+		viewport: viewport,
+		write: write
 	});
 
 	var require$$0 = /*@__PURE__*/getAugmentedNamespace(lib);
@@ -4247,7 +4246,6 @@
 			    // if false, we use the backdrop helper without adding any element to the dom
 			    rootElement: 'body' // give the choice to place backdrop under different elements
 			  };
-
 			  const DefaultType = {
 			    className: 'string',
 			    clickCallback: '(function|null)',
@@ -4396,7 +4394,6 @@
 			    autofocus: true,
 			    trapElement: null // The element to trap focus inside of
 			  };
-
 			  const DefaultType = {
 			    autofocus: 'boolean',
 			    trapElement: 'element'
