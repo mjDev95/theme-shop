5a038bd7cb5520ac453cd3a2eb4e51b4466f0faa
Mario joaquin <mgalicia@denumeris.com>
2025-09-20 21:02:10 -0600

    Ajustes movil

--- global-templates/navbar-branding.php
+++ global-templates/navbar-branding.php
@@ -9,25 +9,28 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-if ( ! has_custom_logo() ) { ?>
+if ( ! has_custom_logo() ) : ?>
 
-	<?php if ( is_front_page() && is_home() ) : ?>
+    <?php if ( is_front_page() && is_home() ) : ?>
 
-		<h1 class="navbar-brand mb-0">
-			<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-				<?php bloginfo( 'name' ); ?>
-			</a>
-		</h1>
+        <h1 class="navbar-brand mb-0 ">
+            <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+                <?php bloginfo( 'name' ); ?>
+            </a>
+        </h1>
 
-	<?php else : ?>
+    <?php else : ?>
 
-		<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-			<?php bloginfo( 'name' ); ?>
-		</a>
+        <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+            <?php bloginfo( 'name' ); ?>
+        </a>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
-	<?php
-} else {
-	the_custom_logo();
-}
+<?php else : ?>
+
+    <div class="logo-responsive mx-auto ms-md-0 me-md-auto mb-3 mb-md-0">
+        <?php the_custom_logo(); ?>
+    </div>
+
+<?php endif; ?>
