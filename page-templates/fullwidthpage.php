3862157c8da4924b8a66f02aeab8625bdc4d7c84
Mario joaquin <mgalicia@denumeris.com>
2025-09-21 21:15:31 -0600

    espacios

--- page-templates/fullwidthpage.php
+++ page-templates/fullwidthpage.php
@@ -12,13 +12,14 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
-if ( is_front_page() ) {
-	$container = '';
-}
 
 if ( is_front_page() ) {
-	get_template_part( 'global-templates/hero' );
+	$container = 'cointainer-fluid px-0';
 }
+
+/*if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}*/
 
 $wrapper_id = 'full-width-page-wrapper';
 if ( is_page_template( 'page-templates/no-title.php' ) ) {
