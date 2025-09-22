056ce8506df825ad269b0f7b305ce16064c0e464
Mario joaquin <mgalicia@denumeris.com>
2025-09-21 21:30:03 -0600

    correcion de espacios

--- page-templates/fullwidthpage.php
+++ page-templates/fullwidthpage.php
@@ -14,7 +14,7 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 
 if ( is_front_page() ) {
-	$container = 'cointainer-fluid px-0';
+	$container = '';
 }
 
 /*if ( is_front_page() ) {
