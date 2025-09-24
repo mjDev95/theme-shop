1c819d6017fc2ed7aed3037b888c7ea95cfd6e45
Mario joaquin <mgalicia@denumeris.com>
2025-09-24 00:09:29 -0600

    Ajustes en el carrito

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -22,15 +22,15 @@ do_action( 'woocommerce_before_cart' ); ?>
 <form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
 
-	<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents table table-hover table-borderless rounded-4 overflow-hidden" cellspacing="0" style="border-radius:1.5rem;">
-		<thead class="bg-info bg-opacity-10">
+	<table class="shop_table shop_table_responsive border-0 cart woocommerce-cart-form__contents table table-hover table-borderless rounded-4 overflow-hidden" cellspacing="0" style="border-radius:1.5rem;">
+		<thead class="bg-info h5">
 		   <tr>
-			   <th class="product-remove"></th>
-			   <th class="product-thumbnail"></th>
-			   <th scope="col" class="product-name fw-semibold text-info-emphasis"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-			   <th scope="col" class="product-price fw-semibold text-info-emphasis"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
-			   <th scope="col" class="product-quantity fw-semibold text-info-emphasis"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
-			   <th scope="col" class="product-subtotal fw-semibold text-info-emphasis"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+			<th class="product-thumbnail"></th>
+			<th scope="col" class="product-name fw-semibold text-info-emphasis"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+			<th scope="col" class="product-price fw-semibold text-info-emphasis"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+			<th scope="col" class="product-quantity fw-semibold text-info-emphasis"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+			<th scope="col" class="product-subtotal fw-semibold text-info-emphasis"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+			<th class="product-remove"></th>
 		   </tr>
 		</thead>
 		<tbody>
@@ -56,25 +56,6 @@ do_action( 'woocommerce_before_cart' ); ?>
 					   $row_class = ($row_index % 2 === 0) ? 'bg-info bg-opacity-10' : 'bg-white';
 					   ?>
 					   <tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?> <?php echo $row_class; ?> align-middle">
-
-						<td class="product-remove">
-							   <?php
-							   $row_index++;
-								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-									'woocommerce_cart_item_remove_link',
-									sprintf(
-										'<a role="button" href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-										/* translators: %s is the product name */
-										esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
-										esc_attr( $product_id ),
-										esc_attr( $_product->get_sku() )
-									),
-									$cart_item_key
-								);
-							?>
-						</td>
-
 						<td class="product-thumbnail">
 						<?php
 						/**
@@ -98,7 +79,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
 						}
 						?>
-						</td>
+
 
 						<td scope="row" role="rowheader" class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
 						<?php
@@ -160,6 +141,23 @@ do_action( 'woocommerce_before_cart' ); ?>
 						<td class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
 							<?php
 								echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+							?>
+						</td>
+						<td class="product-remove">
+							<?php
+							$row_index++;
+							echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+								'woocommerce_cart_item_remove_link',
+								sprintf(
+									'<a role="button" href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+									esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+									/* translators: %s is the product name */
+									esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
+									esc_attr( $product_id ),
+									esc_attr( $_product->get_sku() )
+								),
+								$cart_item_key
+							);
 							?>
 						</td>
 					</tr>
